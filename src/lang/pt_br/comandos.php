b86caea90f8ea26dcdea5fc62bf6d6644e264c7e
Aryan Gomes <aryan.gomes@gmail.com>
2022-05-24 11:54:25 -0300

    feat: Adiciona mensagens padronizadas

---
+++ src/lang/pt_br/comandos.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'preco_recente' => "O preço mais recente de :criptomoeda é US$ :preco_lance",
+    'criptomoeda_invalida' => "Criptomoeda informada :criptomoeda não existe ou é inválida!",
+    'nao_ha_registros_criptomoeda' => "Não há registros da criptomoeda :criptomoeda cadastrada no banco de dados!",
+    'preco_medio_criptomoeda' => "O preço médio da criptomoeda :criptomoeda é US$ :precoMedioDaCriptomoeda",
+    'preco_atual_menor_que_preco_medio_criptomoeda' => "O preço mais recente(US$ :precoMaisRecenteDaCriptomoeda) está menor do que 0.5% do que o preço médio(US$ :precoMedioDaCriptomoeda)!",
+];
